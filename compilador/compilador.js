6fec04cc057b229b888b049a065a28026b651165
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-09 14:44:09 -0300

    add compiler study

---
+++ compilador/compilador.js
@@ -0,0 +1,10 @@
+"use strict";
+let canal = 'Gaveta';
+let inscritos = 610234;
+// canal = inscritos;
+console.log(`canal = ${canal}`);
+// let nome = 'Pedro';
+function soma(a, b) {
+    return a + b;
+}
+;
---
+++ compilador/compilador.ts
@@ -0,0 +1,11 @@
+let canal: string = 'Gaveta';
+let inscritos: number = 610234;
+
+// canal = inscritos;
+console.log(`canal = ${canal}`);
+
+// let nome = 'Pedro';
+
+function soma(a: any, b: any) {
+  return a + b;
+};
