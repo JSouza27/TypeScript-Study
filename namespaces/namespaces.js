4025f4e405fa24334812de57d6b4cb8c0ea32282
Jonathan Souza <souzajonathan570@gmail.com>
2022-03-07 18:00:56 -0300

    add study on modules

---
+++ namespaces/namespaces.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log(Geometria.Areas.circunferencia(10));
+console.log(Geometria.Areas.retangulo(10, 20));
---
+++ namespaces/namespaces.ts
@@ -0,0 +1,2 @@
+console.log(Geometria.Areas.circunferencia(10));
+console.log(Geometria.Areas.retangulo(10, 20));
